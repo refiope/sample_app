d6bea39a643b5c45aa1c0cef06594fdb5f9017a2
Patrick Kang <wintsunlight@gmail.com>
2018-06-04 16:22:12 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,9 @@ class UsersController < ApplicationController
 
     if @user.save
       # Handle a successful save.
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-
-
     else
       render 'new'
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
+    
     assert_template 'users/new'
     assert_select "div.alert-danger", "The form contains 4 errors."
     assert_select "div#error_explanation"
@@ -20,18 +21,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "valid signup information" do
-  get signup_path
+    get signup_path
 
-  assert_difference 'User.count', 1 do
-    post users_path, params: { user: { name:  "Example User",
-                                       email: "user@example.com",
-                                       password:              "password",
-                                       password_confirmation: "password" } }
-  end
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
 
-  follow_redirect!
-  assert_template 'users/show'
-  assert_not flash.empty?
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert is_logged_in?
   end
 
 end
